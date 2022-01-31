16dcb564ea44d11be7f2a3188851cab7f9e28140
paderborner2019 <paderborner2019@gmail.com>
2022-01-31 16:24:20 +0200

    try deploy6

--- src/dataAccess/database/connectiondb.ts
+++ src/dataAccess/database/connectiondb.ts
@@ -5,7 +5,7 @@ import logger from '../../helpers/Logging';
 
 env.config()
 env.config({ path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`) })
-const connectionString = "mongodb+srv://Vladimir:25012005@cluster0.xnyh3.mongodb.net/notebookdb?retryWrites=true&w=majority"//process.env.CONNECTION_STRING !== undefined?process.env.CONNECTION_STRING: "" ;
+const connectionString = process.env.CONNECTION_STRING !== undefined?process.env.CONNECTION_STRING: "" ;
 export function connectdb() {
        mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true }).then(()=> {
               logger.info(`System connected to the database @ ${connectionString} .`);
